dc3cd4ebd99b8c1aef0954110261a9382aba4db4
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2023-12-18 19:22:41 +0100

    add state stopwatch and time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Hero from './components/Hero/Hero'
 import Container from './components/Container/Container';
 import Button from './components/Button/Button';
+import { useState } from 'react';
 
 const App = () => {
 
+  /* W komponencie App dodaj dwie informacje do stanu.
+  Jedna będzie tyczyć się aktualnej wartości odliczanego czasu
+  (ustal jako wartość startową zero),
+  a druga przyda nam się do przypisania intervalu (wartość startowa null). */
+  const [time, setTime] = useState(0);
+  const [stopwatch, setStopwatch] = useState(null);
 
   
+
   return (
     <Container>
       <Hero />
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,5 +1,12 @@
 import styles from './Timer.module.scss';
 
+/* Stwórz dodatkowy komponent, którego zadaniem będzie renderowanie odliczanego czasu.
+Komponent ten powinien przyjmować w formie parametru liczbę milisekund,
+a następnie renderować czas w formacie HH:mm:ss.ms.
+Do konwersji na poprawny format możesz użyć dodatkowej funkcji, 
+ oczywiście musisz napisać od zera. Jak powinna wyglądać funkcja,
+ która otrzymuje czas w miliesekundach, a zwraca czas w formacie HH:mm:ss.ms?
+ Bardzo pomocny może być tutaj wujek Google ;) */
 const timer = () => {
-    
+
 }
